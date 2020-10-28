63f12982e17d4113448fe41ff10eb96693df16ce
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-10-28 02:13:07 -0500

    Add Body component to main App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './Navbar';
+import Body from './Body';
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      { /* Body */ }
+      <Body />
     </div>
   );
 }
