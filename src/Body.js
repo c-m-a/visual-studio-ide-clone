d4bf374ca8d297e44743f9594723a4b549f2fe27
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-10-28 02:12:32 -0500

    Create Body component

---
+++ src/Body.js
@@ -0,0 +1,13 @@
+import './Body.css';
+
+function Body() {
+  return (
+    <div className="body">
+      { /* leftColumn */ }
+      { /* centerColumn */ }
+      { /* rightColumn */ }
+    </div>
+  );
+}
+
+export default Body;
