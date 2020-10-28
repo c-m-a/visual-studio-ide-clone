508f1bbda8bd35fc89c97ac74fc3c4035755d4ea
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-10-28 02:58:50 -0500

    Add styles to left, middle and right column

--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,49 @@
+import CloseIcon from '@material-ui/icons/Close';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import './Body.css';
 
 function Body() {
   return (
     <div className="body">
-      { /* leftColumn */ }
-      { /* centerColumn */ }
-      { /* rightColumn */ }
+      <div className="bodyLeft">
+        <div className="bodyLeftHeader bodyLeftHeaderActive">
+          <div className="bodyLeftTitle">
+            Cloud Explorer
+          </div>
+          <div className="bodyLeftIcons">
+            <KeyboardArrowDownIcon />
+            <CloseIcon />
+          </div>
+        </div>
+        <div className="bodyLeftContainer">
+        </div>
+      </div>
+      <div className="bodyMiddle">
+        <div className="bodyMiddleHeader">
+          <div className="bodyLeftTitle">
+            Calendar
+          </div>
+          <div className="bodyLeftIcons">
+            <KeyboardArrowDownIcon />
+            <CloseIcon />
+          </div>
+        </div>
+        <div className="bodyLeftContainer">
+        </div>
+      </div>
+      <div className="bodyRight">
+        <div className="bodyRightHeader">
+          <div className="bodyLeftTitle">
+            Solution Explorer
+          </div>
+          <div className="bodyLeftIcons">
+            <KeyboardArrowDownIcon />
+            <CloseIcon />
+          </div>
+        </div>
+        <div className="bodyLeftContainer">
+        </div>
+      </div>
     </div>
   );
 }
